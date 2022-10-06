89e2543365b8abf06794ace23d5b37a83bdef62f
lxhdukuai <104820562+lxhdukuai@users.noreply.github.com>
2022-10-06 11:48:19 +0800

    Delete src/main directory

--- src/main/java/io/github/bedwarskit/Util/ColorString.java
+++
@@ -1,9 +0,0 @@
-package io.github.bedwarskit.Util;
-
-import org.bukkit.ChatColor;
-
-public class ColorString {
-    public static String t(String text) {
-        return ChatColor.translateAlternateColorCodes('&', text);
-    }
-}
